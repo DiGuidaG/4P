3f65025f3d861bae1d0f83ad96e451edce0a56b9
DiGuidaG <148056018+DiGuidaG@users.noreply.github.com>
2023-10-18 17:05:37 +0200

    Add files via upload

---
+++ 02-Tabella/Tabella.java
@@ -0,0 +1,11 @@
+
+public class Tabella {
+	public static void main(String[] args) {
+		System.out.println("Persona Altezza (cm)\tNumero di scarpe");
+		System.out.println("======================================");
+		System.out.println("Luisa\t160\t\t36");
+		System.out.println("Marco\t180\t\t43");
+		System.out.println("Giulia\t168\t\t38");
+		
+	}
+}
